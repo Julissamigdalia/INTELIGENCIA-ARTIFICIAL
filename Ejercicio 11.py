7fcd7425dfe10113d1e738e5a6bc24005a2814dc
Julissamigdalia <julissamigdaliaj@gmail.com>
2025-02-11 15:09:27 -0600

    Ejercicio 11

---
+++ Ejercicio 11.py
@@ -0,0 +1,15 @@
+
+num1 = float(input("Ingresa el primer número: "))
+num2 = float(input("Ingresa el segundo número: "))
+
+
+resta = num1 - num2
+multiplicacion = num1 * num2
+
+
+print(f"\nEl resultado de la resta de {num1} - {num2} es: {resta}")
+print(f"\nEl resultado de la multiplicación de {num1} * {num2} es: {multiplicacion}")
+
+
+print("\n¡Programa realizado por Migdalia!")
+1
