fe21c9cf8ce570363eaef60fafeb8149bd760af2
Julissamigdalia <julissamigdaliaj@gmail.com>
2025-02-11 14:24:41 -0600

    Tercer Ejercicio

---
+++ Ejercicio3.py
@@ -0,0 +1,11 @@
+# Programa para sumar dos números ingresados por el usuario
+
+# Pedir al usuario que ingrese los números
+num1 = 5
+num2 = 6
+
+# Sumar los números
+resultado = num1 + num2
+
+# Mostrar el resultado
+print(resultado)
